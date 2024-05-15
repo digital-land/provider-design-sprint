a949b0fba7b6e70cb7b2e922355c3de85a40f834
George Goodall <ggoodall@scottlogic.com>
2024-05-15 11:12:11 +0100

    added check start page

--- app/filters.js
+++ app/filters.js
@@ -3,14 +3,14 @@
 // https://prototype-kit.service.gov.uk/docs/filters
 //
 
-// const xGovFilters = require('@x-govuk/govuk-prototype-filters')
+const xGovFilters = require('@x-govuk/govuk-prototype-filters')
 
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
 
-// const { govukMarkdown } = xGovFilters
+const { govukMarkdown } = xGovFilters
 
-// addFilter('govukMarkdown', govukMarkdown)
+addFilter('govukMarkdown', govukMarkdown)
 
 // Add your filters here
 
