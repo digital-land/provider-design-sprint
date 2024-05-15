8ee4c9fcaa043643a4136f652f11a8d0102176db
George Goodall <ggoodall@scottlogic.com>
2024-05-15 12:32:11 +0100

    add column mapping

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,32 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+router.post('/columnMapping/upload-method', (req, res) => {
+    if(req.body.check.uploadMethod == 'file') {
+      res.redirect('/columnMapping/upload')
+    } else {
+      res.redirect('/columnMapping/url')
+    }
+})
+
+router.post('/columnMapping/upload', (req, res) => {
+    res.redirect('/columnMapping/columns')
+})
+
+router.post('/columnMapping/url', (req, res) => {
+    res.redirect('/columnMapping/columns')
+})
+
+router.post('/columnMapping/geometry', (req, res) => {
+    res.redirect('/columnMapping/start-date')
+})
+
+router.post('/columnMapping/start-date', (req, res) => {
+    res.redirect('/columnMapping/end-date')
+})
+
+router.post('/columnMapping/end-date', (req, res) => {
+    res.redirect('/columnMapping/submit')
+})
+
 // Add your routes here
