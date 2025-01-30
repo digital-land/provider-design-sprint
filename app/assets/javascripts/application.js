5e696c99d9a447bde04e511570e29b414baefeb0
Alex Torrance <alex.torrance@tpximpact.com>
2025-01-30 10:37:22 +0000

    Add javascript to toggle completed status on get started page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,4 +5,28 @@
 
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
+  function toggleComplete(button) {
+    // find tag within parent element
+    const tag = button.closest('.govuk-task-list__item').querySelector('.govuk-task-list__status>strong')
+    const classes = tag.classList;
+    if (tag.textContent.trim() == "Incomplete") {
+      tag.textContent = "Completed"
+      button.textContent = "Mark this step as incomplete"
+      classes.replace("govuk-tag--grey", "govuk-tag--blue")
+    } else {
+      tag.textContent = "Incomplete"
+      button.textContent = "Mark this step as complete"
+      classes.replace("govuk-tag--blue", "govuk-tag--grey")
+    }
+  }
+
+  const markCompletedButtons = document.querySelectorAll('.js-mark-completed');
+  markCompletedButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      console.log('click')
+      toggleComplete(button)
+    },
+    false
+    )
+  })
 })
