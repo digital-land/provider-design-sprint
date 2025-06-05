1c780ef004b130334bda8fffd5bf6f2606600cf2
Alex Torrance <alex.torrance@tpximpact.com>
2025-06-05 15:09:21 +0100

    Add issue detail page

--- app/routes/out-of-bounds.js
+++ app/routes/out-of-bounds.js
@@ -280,8 +280,6 @@ router.get("/organisations/:orgId/:datasetId/tasklist", async (req, res) => {
     res.locals.outOfBoundsEntities = [];
   }
 
-  console.log(res.locals.outOfBoundsEntities);
-
   res.render("/out-of-bounds/tasklist");
 })
 
@@ -325,7 +323,6 @@ router.get("/organisations/:orgId/:datasetId/issue-table", async (req, res) => {
   if (res.locals.outOfBoundsEntities.length > 0) {
     // Use parameterized query for safety
     const placeholders = res.locals.outOfBoundsEntities.map((_, i) => `:id${i}`).join(', ');
-    console.log(placeholders)
     entitiesQuery = {
       sql: `select *
         from entity
@@ -367,6 +364,142 @@ router.get("/organisations/:orgId/:datasetId/issue-table", async (req, res) => {
   }
 
   res.locals.datasetEntities = entitiesResults.rows || [];
+  res.locals.entityCount = res.locals.datasetEntities.length;
 
   res.render("/out-of-bounds/issue-table");
+})
+
+router.get("/organisations/:orgId/:datasetId/issue-detail/:entityId", async (req, res) => {
+  res.locals.version_path = version;
+  res.locals.organisation = getOrg(req.params.orgId);
+  res.locals.dataset = getDataset(req.params.datasetId);
+
+  const entitySql = {
+    sql: `SELECT * FROM entity WHERE entity = :p0`,
+    p0: req.params.entityId,
+    _shape: 'objects'
+  }
+  
+  const entityResponse = await queryDatasette(entitySql, res.locals.dataset.dataset, 'json');
+
+  if (entityResponse.rows && entityResponse.rows.length > 0) {
+    // Parse the json property of each item and add the parsed properties to the item
+    if (entityResponse.rows) {
+      entityResponse.rows.forEach(item => {
+        if (item.json) {
+          try {
+            const parsed = JSON.parse(item.json);
+            Object.assign(item, parsed);
+          } catch (e) {
+            console.error('Failed to parse item.json:', e, item.json);
+          }
+        }
+        // Change underscores in property names to hyphens
+        Object.keys(item).forEach(key => {
+          if (key.includes('_')) {
+            const newKey = key.replace(/_/g, '-');
+            item[newKey] = item[key];
+            delete item[key];
+          }
+        });
+      });
+    }
+
+    res.locals.entity = entityResponse.rows[0];
+
+    // Get out of bounds entities from this dataset for pagination
+    const outOfBoundsQuery= {
+      sql: `select * from expectation
+        where
+          "name" = :p0
+          and "organisation" = :p1
+          and "dataset" = :p2`,
+      p0: 'Check no entities are outside of the local planning authority boundary',
+      p1: res.locals.organisation.organisation,
+      p2: res.locals.dataset.dataset,
+      _shape: 'objects'
+    }
+
+    const outOfBoundsResults = await queryDatasette(outOfBoundsQuery);
+
+    if (outOfBoundsResults.rows && outOfBoundsResults.rows.length > 0) {    
+      const OutOfBoundsDetails = JSON.parse(outOfBoundsResults.rows[0].details);
+      res.locals.outOfBoundsEntities = OutOfBoundsDetails.entities || [];    
+    } else {
+      res.locals.outOfBoundsEntities = [];
+    }
+
+    // Find the index of the current entity in the allEntities array
+    res.locals.currentEntityIndex = res.locals.outOfBoundsEntities.findIndex(e => e == req.params.entityId);
+  } else {
+    res.locals.entity = null;
+  }
+
+  const pageNum = res.locals.currentEntityIndex + 1 || 1;
+  const numEntries = res.locals.outOfBoundsEntities.length;
+
+  const paginationObj = {}
+  if (pageNum > 1) {
+    paginationObj.prevObj = {
+      href: version + req.path.replace(/(\d+)/, res.locals.outOfBoundsEntities[pageNum - 2])
+    }
+  }
+
+  if (pageNum < numEntries) {
+    paginationObj.nextObj = {
+      href: version + req.path.replace(/(\d+)/, res.locals.outOfBoundsEntities[pageNum])
+    }
+  }
+
+  paginationObj.items = []
+  let prevSkip = false;
+  let nextSkip = false;
+  for (let i=1; i<=numEntries; i++) {
+
+    if (i == 1
+      || (i >= pageNum-2 && i <= pageNum+2)
+      || i == numEntries) {
+      
+      let item = {
+        number: i,
+        href: version + req.path.replace(/(\d+)/, res.locals.outOfBoundsEntities[i-1])
+      }
+
+      if (i == pageNum) item.current = true;
+      paginationObj.items.push(item)
+    }
+    
+    if (i > 1 && (i <= pageNum-2) && !prevSkip) {
+      let item = {
+        ellipsis: true
+      }
+
+      paginationObj.items.push(item)
+      prevSkip = true
+    }
+    
+    if (i < numEntries && (i >= pageNum+2) && !nextSkip) {
+      let item = {
+        ellipsis: true
+      }
+
+      paginationObj.items.push(item)
+      nextSkip = true
+    }
+  }
+
+  res.locals.pagination_obj = paginationObj;
+
+  // get column names for the table
+  const columnsQuery = {
+    sql: `select distinct field
+      from column_field
+      where field != "IGNORE"
+      order by rowid`,
+    _shape: 'array'
+  }
+
+  res.locals.columns = await queryDatasette(columnsQuery, res.locals.dataset.dataset);
+
+  res.render("/out-of-bounds/issue-detail");
 })
